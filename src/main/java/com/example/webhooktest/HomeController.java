a23dca10074d811e4f5874ad0ac25567cb41fcd8
varnima <varnimagupta@gmail.com>
2025-05-11 18:10:14 +0530

    homecontroller

---
+++ src/main/java/com/example/webhooktest/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.webhooktest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Webhook application is running!";
+    }
+}
+
